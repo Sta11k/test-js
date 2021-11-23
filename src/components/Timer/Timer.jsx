abbbc425b8a2ebe0c8e5c2a06cf0e99fdda1b1c1
Sta11k <Alexandiel1984@gmail.com>
2021-11-23 17:11:39 +0200

    addClick

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,106 +1,77 @@
-import { useState } from 'react';
-import { observable, Observable, timer } from 'rxjs';
-
-// import { of } from 'rxjs';
-// import { map } from 'rxjs/operators';
-
-// import { timer, of,Observable } from 'rxjs';
-// import { concatMapTo } from 'rxjs/operators';
+import { useState, useEffect } from 'react';
+import { interval, Observable, Subject, timer } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 
 export default function Timer() {
-  const [time, setTime] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [status, setStatus] = useState('stop');
 
-  const hendleStartTimer = e => {
+  // const newLocal = [status];
+  useEffect(() => {
+    const unsubscribe$ = new Subject();
+    interval(100)
+      .pipe(takeUntil(unsubscribe$))
+      .subscribe(() => {
+        if (status === 'start') {
+          setSeconds(val => val + 1000);
+        }
+      });
+    console.log(seconds);
+    return () => {
+      unsubscribe$.next();
+      unsubscribe$.complete();
+    };
+  }, [seconds, status]);
+
+  const HendlStartTimer = e => {
     e.preventDefault();
-    const source = timer(1000);
-    source.subscribe(data => setTime(data));
-    console.log('time', time);
-
-    // source.subscribe(data =>setTime(data));
-    // console.log(time);
-    // timer(due: number | Date, scheduler?: SchedulerLike): Observable<0>;
+    setStatus('start');
   };
 
-  // const targetDate = new Date();
-  //     const nowDate = Date.now();
-  //       const timer = targetDate - nowDate;
-  //       console.log(timer);
+  const HendleStopTimer = e => {
+    setStatus('stop');
+    setSeconds(0);
+  };
 
-  // const timerHour = () => (Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-  // console.log("timerHour",timerHour);
-  //      const timerMin =()=>  (Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  //  const timerSec =()=> (Math.floor((time % (1000 * 60)) / 1000));
-  const stream$ = new Observable(observer => {});
-  const source = timer(1000);
-  const subcription = stream$.subscribe({
-    next: value => source.subscribe(data => setTime(data)),
-  });
+  const HendleResetTimer = e => {
+    setSeconds(0);
+    setStatus('start');
+  };
 
-  console.log(subcription);
+  const HendleWaitTimer = e => {
+    setStatus('wait');
+  };
 
+  console.log('object', seconds);
   return (
-    <>
-      <ul>
-        {/* <li>HH: { timerHour()}</li>
-        <li>MM:{timerMin() }</li> */}
-        <li>SS:{time}</li>
-        {/* <li className={timer__item}>HH:{}</li>
-        <li className={timer__item}>MM:</li>
-        <li className={timer__item}>SS:</li> */}
-      </ul>
-      <ul>
-        <li>
-          <button onClick={hendleStartTimer}>«Start / Stop»</button>
+    <div className="container">
+      <span> {new Date(seconds).toISOString().slice(11, 19)}</span>
+      <ul className="button">
+        <li className="button__item">
+          <button name="start" onClick={HendlStartTimer}>
+            {/* <button name="start" value={name} onClick={HendlStartTimer}> */}
+            «Start / Stop»
+          </button>
         </li>
-        {/* <li className={button__item}>
-           <button onClick={hendelWait}>«Wait»</button>
-         </li>
-         <li className={button__item}>
-           <button onClick={hendeReset}>«Reset»</button>
-         </li>  */}
+        <li className="button__item">
+          <button name="stop" onClick={HendleStopTimer}>
+            {/* <button name="stop" value={name} onClick={HendleStopTimer}> */}
+            «Stop»
+          </button>
+        </li>
+        <li className="button__item">
+          <button name="wait" onClick={HendleWaitTimer}>
+            {/* <button name="wait" value={name} onClick={HendleWaitTimer}> */}
+            «Wait»
+          </button>
+        </li>
+        <li className="button__item">
+          <button name="reset" onClick={HendleResetTimer}>
+            {/* <button name="reset" value={name} onClick={HendleResetTimer}> */}
+            «Reset»
+          </button>
+        </li>
       </ul>
-    </>
+    </div>
   );
 }
-
-// import { observable, Observable } from 'rxjs';
-// import s from './Timer.module.css';
-//  import { timer, interval } from 'rxjs';
-// // import { useState } from 'react';
-// // import { takeUntil } from 'rxjs/operators';
-
-//  function Timer() {
-
-// const timer = new Observable(observer => {
-//   let counter = 0;
-//   const intervalId = setInterval(() => {
-//     observer.next(counter++);
-//   }, 1000);
-//   return () => {
-//    clearInterval(intervalId);
-//   }
-// });
-
-//    console.log(timer)
-//   return (
-//     <>
-//        <ul className={s.timer}>
-//         <li className={s.timer__item}>HH:{}</li>
-//         <li className={s.timer__item}>MM:</li>
-//         <li className={s.timer__item}>SS:</li>
-//       </ul>
-//       <ul className={s.button}>
-//         {/* <li className={s.button__item}>
-//           <button onClick={hendelStartStop}>«Start / Stop»</button>
-//         </li> */}
-//         {/* <li className={s.button__item}>
-//           <button onClick={hendelWait}>«Wait»</button>
-//         </li>
-//         <li className={s.button__item}>
-//           <button onClick={hendeReset}>«Reset»</button>
-//         </li> */}
-//       </ul>
-//     </>
-//   );
-// }
-// export default Timer;
